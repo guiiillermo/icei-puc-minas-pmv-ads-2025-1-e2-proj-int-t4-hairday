1ca2e389b98fb80bb8f39013c1b738949b2576d9
Vitor Bisi Vieira <vitor.bisi@sga.pucminas.br>
2025-04-29 13:14:44 -0300

    Remove arquivos da pasta lib de wwwroot do controle de versão e os adiciona ao .gitignore

---
+++ src/Hairday/Models/Horario_Disponivel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Hairday.Models
+{
+    public class Horario_Disponivel
+    {
+        [Key]
+        public int id_horario { get; set; }
+
+        [Required, ForeignKey("barbeiro")]
+        public string CPF_barbeiro { get; set; }
+
+        public DateTime data { get; set; }
+        public TimeSpan hora { get; set; }
+
+        public bool ocupado { get; set; }
+        public int? id_agendamento { get; set; }
+
+        public Barbeiro barbeiro { get; set; }
+        public Agendamento agendamento { get; set; }
+    }
+}
