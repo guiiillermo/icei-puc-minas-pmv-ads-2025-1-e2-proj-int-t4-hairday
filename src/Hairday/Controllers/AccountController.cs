4644db8bcf735c0eed6c9ab38a292e05c9cd63d4
Bolitaa <marcosfortunato12@outlook.com>
2025-05-08 16:46:32 -0300

    Primeiro commit

---
+++ src/Hairday/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Hairday.Models;  // ajuste o namespace se necessário
+using System.Linq;
+using Hairday.Controllers;
+
+public class AccountController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public AccountController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Login(string email, string senha)
+    {
+        var usuario = _context.Clientes.SingleOrDefault(c => c.email == email && c.senha == senha);
+
+        if (usuario != null)
+        {
+            TempData["Mensagem"] = "Login bem-sucedido!";
+            return RedirectToAction("Index", "Home");
+        }
+
+        ModelState.AddModelError(string.Empty, "Email ou senha inválidos.");
+        return View();
+    }
+}
