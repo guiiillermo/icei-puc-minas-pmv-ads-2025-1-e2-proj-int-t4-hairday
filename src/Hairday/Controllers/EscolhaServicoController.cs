1576c6852c7ce2a176b57b70389fb26f71150a13
Vitor Bisi Vieira <vitor.bisi@sga.pucminas.br>
2025-05-16 13:13:57 -0300

    pagina de selecao de servicos (rascunho)

---
+++ src/Hairday/Controllers/EscolhaServicoController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+//using Hairday.Data;
+using Hairday.Models;
+using Hairday.ViewModels;
+
+namespace Hairday.Controllers
+{
+    public class EscolhaServicoController : Controller
+    {
+        private readonly AppDbContext _context;
+        public EscolhaServicoController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /EscolhaServico/Detalhes?cnpj=11111111000101
+        public IActionResult Detalhes(string cnpj)
+        {
+            if (string.IsNullOrEmpty(cnpj))
+            {
+                return NotFound("CNPJ não informado.");
+            }
+
+            var barbearia = _context.Barbearias.FirstOrDefault(b => b.CNPJ_barbearia == cnpj);
+
+            if (barbearia == null)
+            {
+                return NotFound("Barbearia não encontrada.");
+            }
+
+            var barbeiros = _context.Barbeiros
+                .Where(b => b.CNPJ_barbearia == cnpj)
+                .Include(b => b.servicos)
+                .ToList();
+
+            var vm = new EscolhaServicoViewModel
+            {
+                Barbearia = barbearia,
+                Barbeiros = barbeiros
+            };
+
+            return View(vm);
+        }
+    }
+}
---
+++ src/Hairday/ViewModels/EscolhaServicoViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Hairday.ViewModels
+{
+    using Hairday.Models;
+    using System.Collections.Generic;
+    public class EscolhaServicoViewModel
+    {
+        public Barbearia Barbearia { get; set; }
+        public List<Barbeiro> Barbeiros { get; set; }
+    }
+}
